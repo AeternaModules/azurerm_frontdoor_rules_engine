e58b51e6a3a6b132712f95003a201fec1327df5c
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:33:50 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ EOT
     frontdoor_name      = string
     name                = string
     resource_group_name = string
-    enabled             = optional(bool) # Default: true
+    enabled             = optional(bool)
     rule = optional(list(object({
       action = optional(object({
         request_header = optional(list(object({
@@ -47,7 +47,7 @@ EOT
         })))
       }))
       match_condition = optional(list(object({
-        negate_condition = optional(bool) # Default: false
+        negate_condition = optional(bool)
         operator         = string
         selector         = optional(string)
         transform        = optional(list(string))
